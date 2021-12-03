f641a39a973bada09dc0f914419e4e84a009df18
Luis Carlos <luiscarlosnors@gmail.com>
2021-12-02 21:33:08 -0300

    Feat: Utilizando StyleSheet.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,18 +1,64 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {
+    View,
+    Text,
+    StyleSheet,
+    TextInput,
+    Platform,
+    Button,
+    TouchableOpacity,
+} from 'react-native';
 
 export function Home() {
     return (
-        <View
-            style={{
-                flex: 1,
-                justifyContent: 'center',
-                alignItems: 'center',
-            }}>
-            <Text>React Native</Text>
-            <Text style={{alignSelf: 'center', marginBottom: 20}}>
-                Luis Carlos
-            </Text>
+        <View style={styles.container}>
+            <Text style={styles.text}>Welcome, Luis Carlos</Text>
+            <TextInput
+                style={styles.textInput}
+                placeholder="New Skill"
+                placeholderTextColor="#999"
+            />
+            <TouchableOpacity
+                style={styles.touchableOpacity}
+                activeOpacity={0.7}>
+                <Text style={styles.buttonText}>Add</Text>
+            </TouchableOpacity>
+            <Text style={[styles.text, {marginTop: 50}]}>My Skills</Text>
         </View>
     );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#121015',
+        paddingVertical: 70,
+        paddingHorizontal: 30,
+    },
+    text: {
+        color: 'white',
+        fontSize: 24,
+        fontWeight: 'bold',
+        alignSelf: 'center',
+    },
+    textInput: {
+        backgroundColor: '#1F1E25',
+        color: '#FFF',
+        fontSize: 18,
+        padding: Platform.OS === 'ios' ? 15 : 10,
+        marginTop: 30,
+        borderRadius: 7,
+    },
+    touchableOpacity: {
+        backgroundColor: '#A370F7',
+        padding: 15,
+        borderRadius: 7,
+        alignItems: 'center',
+        marginTop: 20,
+    },
+    buttonText: {
+        color: '#FFF',
+        fontSize: 17,
+        fontWeight: 'bold',
+    },
+});
